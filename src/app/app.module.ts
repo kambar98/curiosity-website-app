aea4f2e6d03ca18445a9b60016f66c766fea824d
kamil <kamilbarul123@gmail.com>
2020-10-14 19:11:27 +0200

    Added 'Add' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { AwardedComponent } from './awarded/awarded.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
-    AwardedComponent
+    AwardedComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule
