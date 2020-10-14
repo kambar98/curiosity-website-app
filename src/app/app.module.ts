47e5e3e7ef1c232ea70fb70c62e88c9ac94a7f54
kamil <kamilbarul123@gmail.com>
2020-10-14 17:47:20 +0200

    Added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { AwardedComponent } from './awarded/awarded.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    AwardedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/awarded/awarded.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-awarded',
+  templateUrl: './awarded.component.html',
+  styleUrls: ['./awarded.component.css']
+})
+export class AwardedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
