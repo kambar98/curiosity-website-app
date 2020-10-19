ded54b46382676915994a9d5353f692aaf9a05c5
kamil <kamilbarul123@gmail.com>
2020-10-19 18:51:20 +0200

    Added sample login forms , popups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AwardedComponent } from './awarded/awarded.component';
 import { AddComponent } from './add/add.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { LogInComponent } from './log-in/log-in.component';
+
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/Główna', pathMatch:'full' },
   { path: 'Główna', component: MainComponent },
   { path: 'Wyróżnione', component: AwardedComponent },
-  { path: 'Dodaj', component: AddComponent },
-  { path: 'Rejestracja', component: RegistrationComponent },
-  { path: 'Logowanie', component: LogInComponent }
+  { path: 'Dodaj', component: AddComponent }
+ 
 
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { AwardedComponent } from './awarded/awarded.component';
 import { AddComponent } from './add/add.component';
-import { LogInComponent } from './log-in/log-in.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 
@@ -18,9 +16,7 @@ import { RouterModule } from '@angular/router';
     MainComponent,
     HeaderComponent,
     AwardedComponent,
-    AddComponent,
-    LogInComponent,
-    RegistrationComponent
+    AddComponent
   ],
   imports: [
     BrowserModule,
