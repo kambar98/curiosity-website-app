ae9a6b9a1348185fc0e30796d6223406f18b56d6
kamil <kamilbarul123@gmail.com>
2020-10-29 19:09:37 +0100

    Added Autologin and AutoLogout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './header/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'curiosity';
+  constructor(private authService: AuthService) {
+
+  }
+  ngOnInit() {
+    this.authService.autoLogin();
+
+  }
 }
--- src/app/header/auth.service.ts
+++ src/app/header/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject } from 'rxjs';
+import { throwError, Subject, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 
 export interface AuthResponseData {
@@ -19,7 +19,8 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 
 export class AuthService {
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
+  private tokenExpirationTime: any;
 
   constructor(private http: HttpClient) { }
 
@@ -43,9 +44,11 @@ export class AuthService {
       }
       return throwError(errorMessage);
     }), tap(responseData => {
-      const expirationDate = new Date(new Date().getTime() + + responseData.expiresIn * 1000);
+      const expirationDate = new Date(new Date().getTime() + +responseData.expiresIn * 1000);
       const user = new User(responseData.email, responseData.localId, responseData.idToken, expirationDate);
       this.user.next(user);
+      this.autoLogout(+ responseData.expiresIn * 1000);
+      localStorage.setItem('userData', JSON.stringify(user));
 
     })
     );
@@ -76,13 +79,48 @@ export class AuthService {
       const expirationDate = new Date(new Date().getTime() + + responseData.expiresIn * 1000);
       const user = new User(responseData.email, responseData.localId, responseData.idToken, expirationDate);
       this.user.next(user);
-
+      this.autoLogout(+ responseData.expiresIn*1000);
+      localStorage.setItem('userData', JSON.stringify(user));
     })
     );
   }
   logout() {
     this.user.next(null);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTime) {
+      clearTimeout(this.tokenExpirationTime);
+    }
+    this.tokenExpirationTime = null;
   }
- 
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTime= setTimeout(() => {
+      this.logout();
+    }
+      , expirationDuration)
+  }
+
+  autoLogin() {
+
+    const userData:{
+    email: string;
+    id: string;
+    _token: string;
+    _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+    if (loadedUser.token) {
+
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+
+  }
+
 }
 
